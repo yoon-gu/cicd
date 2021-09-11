8aa87cfb69e75b0b71f36f80ac5547d8467c8219
Yoon-gu Hwang <yz0624@gmail.com>
2021-09-11 09:19:55 +0900

    new version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(	name='wolfgang', version='0.0',
+setup(	name='wolfgang', version='0.1',
 		description='CI/CD Example',
 		url='https://github.com/yoon-gu/ci-cd/',
 		author='Yoon-gu Hwang',
